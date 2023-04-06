d23b7c82fbf12f909245c6128b1ed0c03646aa22
Vegard Hillestad <vegard.lundeberg.hillestad@nav.no>
2023-04-06 09:13:38 +0200

    updated db settings

--- eux-relaterte-rinasaker-webapp/src/main/kotlin/no/nav/eux/relaterte/rinasaker/DataSourceProperties.kt
+++ eux-relaterte-rinasaker-webapp/src/main/kotlin/no/nav/eux/relaterte/rinasaker/DataSourceProperties.kt
@@ -1,8 +1,9 @@
 package no.nav.eux.relaterte.rinasaker
 
+import com.zaxxer.hikari.HikariConfig
 import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties(prefix = "datasource")
 data class DataSourceProperties(
-    val hostname: String,
+    var hikari: HikariConfig
 )
