6264ccffef7115198f475344e14e95a2f319ab1a
Vegard Hillestad <vegard.lundeberg.hillestad@nav.no>
2023-04-17 14:30:30 +0200

    updated index.html

--- eux-relaterte-rinasaker-webapp/src/main/kotlin/no/nav/eux/relaterte/rinasaker/Application.kt
+++ eux-relaterte-rinasaker-webapp/src/main/kotlin/no/nav/eux/relaterte/rinasaker/Application.kt
@@ -7,9 +7,11 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Import
 
-@EnableJwtTokenValidation(ignore = [
-    "org.springframework",
-    "org.springdoc"]
+@EnableJwtTokenValidation(
+    ignore = [
+        "org.springframework",
+        "org.springdoc"
+    ]
 )
 @SpringBootApplication
 @Import(value = [SpringDocConfiguration::class])
