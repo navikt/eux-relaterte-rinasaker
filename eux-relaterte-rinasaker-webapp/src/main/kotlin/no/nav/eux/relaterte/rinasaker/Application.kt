3d91f91dcfc9dee3fc830b589c351cfe04dd8918
Vegard Hillestad <vegard.lundeberg.hillestad@nav.no>
2023-04-17 13:07:30 +0200

    remove token validation

--- eux-relaterte-rinasaker-webapp/src/main/kotlin/no/nav/eux/relaterte/rinasaker/Application.kt
+++ eux-relaterte-rinasaker-webapp/src/main/kotlin/no/nav/eux/relaterte/rinasaker/Application.kt
@@ -7,10 +7,6 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Import
 
-@EnableJwtTokenValidation(ignore = [
-    "org.springframework",
-    "springfox.documentation.swagger.web"]
-)
 @SpringBootApplication
 @Import(value = [SpringDocConfiguration::class])
 @EnableConfigurationProperties(DataSourceProperties::class)
