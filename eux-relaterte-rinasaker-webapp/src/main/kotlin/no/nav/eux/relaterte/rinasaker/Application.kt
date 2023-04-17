dc99924084a24c738569c51db6ac28c2e46a6c71
Vegard Hillestad <vegard.lundeberg.hillestad@nav.no>
2023-04-17 12:51:45 +0200

    remove jwt validation for springfox.documentation.swagger.web

--- eux-relaterte-rinasaker-webapp/src/main/kotlin/no/nav/eux/relaterte/rinasaker/Application.kt
+++ eux-relaterte-rinasaker-webapp/src/main/kotlin/no/nav/eux/relaterte/rinasaker/Application.kt
@@ -7,7 +7,10 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Import
 
-@EnableJwtTokenValidation
+@EnableJwtTokenValidation(ignore = [
+    "org.springframework",
+    "springfox.documentation.swagger.web"]
+)
 @SpringBootApplication
 @Import(value = [SpringDocConfiguration::class])
 @EnableConfigurationProperties(DataSourceProperties::class)
