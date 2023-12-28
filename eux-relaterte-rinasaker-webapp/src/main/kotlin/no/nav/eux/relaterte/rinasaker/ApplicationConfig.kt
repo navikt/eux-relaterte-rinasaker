2595a438dd51e1e63462e89bcf296ef51ee5c4e4
Vegard Hillestad <vegard.lundeberg.hillestad@nav.no>
2023-12-28 19:03:33 +0100

    request mdc filter

--- eux-relaterte-rinasaker-webapp/src/main/kotlin/no/nav/eux/relaterte/rinasaker/ApplicationConfig.kt
+++ eux-relaterte-rinasaker-webapp/src/main/kotlin/no/nav/eux/relaterte/rinasaker/ApplicationConfig.kt
@@ -1,14 +1,19 @@
 package no.nav.eux.relaterte.rinasaker
 
 import com.zaxxer.hikari.HikariDataSource
+import no.nav.eux.logging.RequestIdMdcFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class PersistenceConfig(
+class ApplicationConfig(
     val dataSourceProperties: DataSourceProperties
 ) {
 
     @Bean
     fun hikariDataSource() = HikariDataSource(dataSourceProperties.hikari)
+
+    @Bean
+    fun requestIdMdcFilter() = RequestIdMdcFilter()
+    
 }
