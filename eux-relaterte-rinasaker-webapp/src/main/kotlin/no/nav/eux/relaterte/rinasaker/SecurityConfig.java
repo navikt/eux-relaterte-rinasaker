2eade9ecf9815d84506df5fec8b2ce944eceeb99
Vegard Hillestad <vegard.lundeberg.hillestad@nav.no>
2025-08-07 14:26:00 +0200

    probe

---
+++ eux-relaterte-rinasaker-webapp/src/main/kotlin/no/nav/eux/relaterte/rinasaker/SecurityConfig.java
@@ -0,0 +1,27 @@
+package no.nav.eux.relaterte.rinasaker;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(authz -> authz
+                .requestMatchers("/actuator/health", "/actuator/health/**").permitAll()
+                .requestMatchers("/swagger-ui/index.html", "/swagger-ui/**").permitAll()
+                .requestMatchers("/actuator/prometheus").permitAll()
+                .anyRequest().authenticated()
+            )
+            .oauth2ResourceServer(oauth2 -> oauth2
+                .jwt(Customizer.withDefaults())
+            );
+        
+        return http.build();
+    }
+}
